8bf68d9b9e4037c9b3d4914656ce6688f97ae6a8
jgarciaanria-dot <jgarcia.anria@gmail.com>
2026-09-15 09:39:39 -0500

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -4,7 +4,7 @@
 // =========================================================
 
 const SUPABASE_URL = 'https://hokrimtsyseuqfjjvmxu.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_...';
+const SUPABASE_KEY = 'sb_publishable_7JZShvbADW0URka-k_hjBQ_MSE0LM-V';
 
 const sbClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
